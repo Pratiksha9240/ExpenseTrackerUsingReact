93e08e1e5efe817c5980945d382b9e1923deae17
Pratiksha9240 <dhavalepratiksha9240@gmail.com>
2022-12-29 19:24:12 +0530

    Initial changes

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,14 @@
+function ExpenseItem() {
+  return (
+    <>
+      <h1>Expense Item</h1>
+      <ol>
+        <li>Food Rs 10</li>
+        <li>Petrol Rs 100</li>
+        <li>Movies Rs 200</li>
+      </ol>
+    </>
+  );
+}
+
+export default ExpenseItem;
