d5dc3bde7a7b7cd0d5204ceae70d99563058b941
Pratiksha9240 <dhavalepratiksha9240@gmail.com>
2022-12-30 12:34:40 +0530

    small changes and css file added

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,14 @@
+import './ExpenseItem.css'
+
 function ExpenseItem() {
   return (
-    <>
-      <h1>Expense Item</h1>
-      <ol>
-        <li>Food Rs 10</li>
-        <li>Petrol Rs 100</li>
-        <li>Movies Rs 200</li>
-      </ol>
-    </>
+    <div className = 'expense-item'>
+      <div>March 28th 2022</div>
+      <div className = 'expense-item__description'>
+        <h2>Car Insurance</h2>
+        <div className = 'expense-item__price'>Rs.10000</div>
+      </div>
+    </div>
   );
 }
 
