dca5a805c64d641fe042d243173a9ac49d0e4433
Pratiksha9240 <dhavalepratiksha9240@gmail.com>
2023-01-03 18:13:03 +0530

    Delete function using DOM Manipulations

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -5,12 +5,20 @@ import Card from '../UI/Card';
 
 const ExpenseItem = (props) => {
   
+  const deleteExpenseHandler = () => {
+    const d = document.getElementById(`${props.date}`);
+    const c = d.remove();
+    console.log(d);
+  }
 
   return (
+    <div id={props.date}>
     <Card className = 'expense-item'>
       <ExpenseDate date = {props.date}></ExpenseDate>
       <ExpenseDetails title = {props.title} amount = {props.amount} location = {props.location}></ExpenseDetails>
+      <button onClick={deleteExpenseHandler}>Delete Expense</button>
     </Card>
+    </div>
   );
 }
 
