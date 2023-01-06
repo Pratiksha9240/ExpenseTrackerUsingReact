b95ff35bede08171b8cc6035d0ddfc4417b6b3e9
Pratiksha9240 <dhavalepratiksha9240@gmail.com>
2023-01-06 11:38:46 +0530

    conditional content added

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,37 +1,58 @@
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
-import './Expenses.css';
+import "./Expenses.css";
 import React, { useState } from "react";
 import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2022");
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
 
-    const [filteredYear,setFilteredYear] = useState('2022');
-    const filterChangeHandler = (selectedYear) => {
-      setFilteredYear(selectedYear);
-    }
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.expenseDate.getFullYear().toString() === filteredYear;
+  });
 
-    const filteredExpenses = props.items.filter(
-      (expense) => {
-        return expense.expenseDate.getFullYear().toString() === filteredYear;
-      }
-    )
+  let expenseContent = <p>No Expense Found.</p>
 
-    return (
-        <Card className = 'expenses'>
-          <ExpensesFilter selected = {filteredYear} onChangeFilter = {filterChangeHandler}></ExpensesFilter>
-        {filteredExpenses.map((ele) => (
-            <React.Fragment key={ele.id}>
-              <ExpenseItem
-                title = {ele.expenseTitle}
-                amount = {ele.expenseAmount}
-                location = {ele.expenseLocation}
-                date = {ele.expenseDate}
-              ></ExpenseItem>
-            </React.Fragment>
-          ))}
-          </Card>
-    )
-}
+  if(filteredExpenses.length > 0){
+    expenseContent = filteredExpenses.map((ele) => (
+      <React.Fragment key={ele.id}>
+        <ExpenseItem
+          title={ele.expenseTitle}
+          amount={ele.expenseAmount}
+          location={ele.expenseLocation}
+          date={ele.expenseDate}
+        ></ExpenseItem>
+      </React.Fragment>
+    ))
+  }
+
+  if(filteredExpenses.length === 1){
+    expenseContent = filteredExpenses.map((ele) => (
+      <React.Fragment key={ele.id}>
+        <ExpenseItem
+          title={ele.expenseTitle}
+          amount={ele.expenseAmount}
+          location={ele.expenseLocation}
+          date={ele.expenseDate}
+        ></ExpenseItem>
+        <p>Only one Expense Found.Please add more.</p>
+      </React.Fragment>
+      
+    ))
+  }
+
+  return (
+    <Card className="expenses">
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      ></ExpensesFilter>
+      {expenseContent}
+    </Card>
+  );
+};
 
 export default Expenses;
